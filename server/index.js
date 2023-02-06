97dfcd6c1d11a962e7250e14cba558b3e59f5b1a
ieatn <ieatnubcake@gmail.com>
2023-02-06 14:58:17 -0800

    basic routes, use postman to check

--- server/index.js
+++ server/index.js
@@ -5,3 +5,16 @@ const app = express()
 app.listen(process.env.PORT, () => {
     console.log('listening to port', process.env.PORT)
 })
+// routes
+app.get('/', (req, res) => {
+    res.json({msg: 'get all'})
+})
+app.post('/', (req, res) => {
+    res.json({msg: 'create'})
+})
+app.delete('/:id', (req, res) => {
+    res.json({msg: 'delete'})
+})
+app.put('/:id', (req, res) => {
+    res.json({msg: 'update'})
+})
