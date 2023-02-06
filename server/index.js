c5a035b71e9bb97d55164593e676a904f50a2b10
ieatn <ieatnubcake@gmail.com>
2023-02-06 15:21:36 -0800

    working CRUD functions and access to database

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const express = require('express')
 const app = express()
+// need for req.body for post request
+app.use(express.json())
 
 // npm i mongoose
 const mongoose = require('mongoose')
@@ -14,17 +16,27 @@ mongoose.connect(process.env.MONGODB_URI)
         })
     })
 
+const Todo = require('./models/TodoModel')
 
-// routes
-app.get('/', (req, res) => {
-    res.json({msg: 'get all'})
+app.get('/', async (req, res) => {
+    const allTodos = await Todo.find({})
+    res.json(allTodos)
 })
-app.post('/', (req, res) => {
-    res.json({msg: 'create'})
+app.post('/', async (req, res) => {
+    const {title} = req.body
+    const todo = await Todo.create({title})
+    res.json(todo)
 })
-app.delete('/:id', (req, res) => {
-    res.json({msg: 'delete'})
+app.delete('/:id', async (req, res) => {
+    const {id} = req.params
+    const todo = await Todo.findById(id)
+    await todo.remove()
+    res.json(todo)
 })
-app.put('/:id', (req, res) => {
-    res.json({msg: 'update'})
+app.put('/:id', async (req, res) => {
+    const {id} = req.params
+    const todo = await Todo.findById(id)
+    todo.title = req.body.title
+    await todo.save()
+    res.json(todo)
 })
