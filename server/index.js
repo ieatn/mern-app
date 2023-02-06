4314b7448b70f9d5e96285a81857ffcf0c4c3d22
ieatn <ieatnubcake@gmail.com>
2023-02-06 14:54:30 -0800

    npm i dotenv

--- server/index.js
+++ server/index.js
@@ -1,5 +1,7 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
-app.listen(3000, () => {
-    console.log('listening to port 3000')
+app.listen(process.env.PORT, () => {
+    console.log('listening to port', process.env.PORT)
 })
