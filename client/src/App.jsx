0c915518ec02f0a26037eb265cc8b72d1b723d22
ieatn <ieatnubcake@gmail.com>
2023-02-10 18:47:28 -0800

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,11 +3,13 @@ import './App.css'
 import Home from './pages/Home'
 import Register from './pages/Register'
 import Login from './pages/Login'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <div className="App">
+      <Navbar />
       <Login />
     </div>
   )
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+const Navbar = () => {
+    return ( 
+        <div className="container">
+            <nav>
+                <div>buddy</div>
+                <div>
+                    <div>Login</div>
+                    <div>Register</div>
+                </div>
+            </nav>
+        </div>
+     );
+}
+ 
+export default Navbar;
