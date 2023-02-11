b2495bb9e175826040d5c3fb099e3dc91837a173
ieatn <ieatnubcake@gmail.com>
2023-02-10 17:35:13 -0800

    register new users in database

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -13,4 +13,16 @@ const userSchema = new Schema({
     }
 })
 
+// register a new user by creating a static function using mongoose model 
+userSchema.statics.register = async function(email, password) {
+    // this keyword only works in regular function 
+    const exists = await this.findOne({email})
+    if (exists) {
+        throw Error('Email already in use')
+    } 
+    const user = await this.create({email, password})
+    
+    return user
+}
+
 module.exports = mongoose.model('User', userSchema)
--- server/routes/usercontroller.js
+++ server/routes/usercontroller.js
@@ -1,10 +1,21 @@
+const User = require('../models/userModel')
+
 // login
 const loginUser = async (req, res) => {
     res.json({msg: 'login'})
 }
 // signup
 const registerUser = async (req, res) => {
-    res.json({msg: 'register'})
+    const {email, password} = req.body
+    try {
+        // call the register method in user model that creates user in db and returns it
+        const user = await User.register(email, password)
+        res.status(200).json({email, user})
+    } catch (error) {
+        // throw error email already in use
+        res.status(400).json({error: error.message})
+    }
+    
 }
 
 module.exports = {registerUser, loginUser}
